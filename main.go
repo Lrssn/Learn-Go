63d8d609d39cc92f88b599646d16ed09cd795451
Lrssn <lrssn95@gmail.com>
2024-06-15 16:50:01 +0200

    Switch
    Switch cases

--- main.go
+++ main.go
@@ -1,21 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	if 8%2 == 0 || 7%2 == 0 { // ||=OR, &&=AND
-		fmt.Println("either 8 or 7 are even")
-	}
-	if 8%2 == 0 && 6%2 == 0 { // ||=OR, &&=AND
-		fmt.Println("both 8 and 6 are even")
+	i := 2 //simple case statement
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
 	}
 
-	if num := 9; num < 0 { // if-else if-else
-		fmt.Println(num, "is negative")
-	} else if num < 10 {
-		fmt.Println(num, "has 1 digit")
-	} else {
-		fmt.Println(num, "has multiple digits")
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default: // default is for all calues that has no specific case
+		fmt.Println("It's a weekday")
 	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12: // case can be any operator not only specific values
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) { // function in function?
+		switch t := i.(type) { // variable type
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
