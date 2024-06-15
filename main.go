8a8d5f03b366696e5921e3c246e4f652b2c33826
Lrssn <lrssn95@gmail.com>
2024-06-15 16:29:48 +0200

    variables
    tut 2 variables

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("The time is", time.Now())
+
+	var a = "initial" // string
+	fmt.Println(a)
+
+	var b, c int = 1, 2 // multiple ints
+	fmt.Println(b, c)
+
+	var d = true // bool
+	fmt.Println(d)
+
+	var e int // empty int
+	fmt.Println(e)
+
+	f := "apple" // auto infer type
+	fmt.Println(f)
 }
