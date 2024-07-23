952c4f00699f953c04470658d3c010244da50e9b
Lrssn <lrssn95@gmail.com>
2024-07-23 16:22:48 +0200

    Recover

---
+++ GoByExample/Tutorial 49- Recover/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func mayPanic() {
+	panic("a problem") // This function panics
+}
+
+func main() {
+
+	defer func() {
+		if r := recover(); r != nil { // Recover has to be defered so it runs after panic
+
+			fmt.Println("Recovered. Error:\n", r) // Return value of recover is the error code from the panic
+		}
+	}()
+
+	mayPanic()
+
+	fmt.Println("After mayPanic()")
+}
